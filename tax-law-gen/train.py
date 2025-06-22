d3c177ca98d504773f499eb0484c808cc33bcb69
prismdata <theprimdata@gmail.com>
2025-06-22 19:58:13 +0900

    ev

--- tax-law-gen/train.py
+++ tax-law-gen/train.py
@@ -245,7 +245,7 @@ def main():
         logging_steps=config.logging_steps,
         save_steps=config.save_steps,
         eval_steps=config.eval_steps,
-        evaluation_strategy="steps",
+        eval_strategy="steps",
         save_strategy="steps",
         load_best_model_at_end=True,
         metric_for_best_model="eval_loss",
